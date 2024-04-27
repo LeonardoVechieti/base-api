b13b549452818bad9a9cf1c6de1ee24798cb84ea
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-04-27 00:06:34 -0300

    add: migrations para posts

---
+++ database/migrations/1714186955250_posts.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Posts extends BaseSchema {
+  protected tableName = 'tab_posts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.text('content').notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
