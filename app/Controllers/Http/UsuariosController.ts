d5ce0932c89d83138c3902e33ebbe010ac3bb08d
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-04-27 00:07:28 -0300

    refactor:  config de pastas do projeto

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,12 +1,13 @@
 import Usuario from 'App/Models/Usuario'
 import { UsuarioRepository } from 'App/Repository/UsuarioRepository'
 import DataPageValidator from 'App/Validators/DataPageValidator'
+import { HttpContext } from '@adonisjs/core/build/standalone'
 
 export default class UsuariosController {
-  // public async data({ request }: HttpContext): Promise<Usuario[]> {
-  //   let body = await request.validate(DataPageValidator)
-  //   return await new UsuarioRepository().dataSearch(body.data, body.page)
-  // }
+  public async data({ request }: HttpContext): Promise<Usuario[]> {
+    let body = await request.validate(DataPageValidator)
+    return await new UsuarioRepository().data(body)
+  }
 
   public async full(): Promise<Usuario[]> {
     return await new UsuarioRepository().full()
--- app/Repository/UsuarioRepository.ts
+++ app/Repository/UsuarioRepository.ts
@@ -5,4 +5,8 @@ export class UsuarioRepository {
   public async full(): Promise<Usuario[]> {
     return Usuario.query().orderBy('created_at', 'desc')
   }
+
+  public async data(body): Promise<Usuario[]> {
+    return Usuario.query().where('name', 'like', `%${body.name}%`).orderBy('created_at', 'desc')
+  }
 }
