7edf31220608cb801943cf785fdeef8955847edd
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-04-27 23:41:34 -0300

    update: rota posts

--- start/routes/post.ts
+++ start/routes/post.ts
@@ -2,6 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('/post', 'PostsController.create')
-  Route.get('/post', 'PostsController.index')
+  Route.get('/post', 'PostsController.getPosts')
 })
 .prefix('api')
